328979261cb007379d2253c93a08c06de84c4275
Daniel Matsumoto <daniel.matsumoto@amcom.com.br>
2019-01-17 10:23:49 -0200

    Configurações para publicaçao em produçao

--- smeCore.API/Program.cs
+++ smeCore.API/Program.cs
@@ -12,6 +12,7 @@ namespace smeCore.API
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseUrls(urls: "http://localhost:57684");
     }
 }
