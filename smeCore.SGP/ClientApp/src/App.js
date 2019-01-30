716997bdfa0f4ba674d084cebcf0ebe6d9136741
Daniel Matsumoto <daniel.matsumoto@amcom.com.br>
2019-01-30 15:40:45 -0200

    [Tela Login]: - Desenvolvimento da tela de login

--- smeCore.SGP/ClientApp/src/App.js
+++ smeCore.SGP/ClientApp/src/App.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 //import { Layout } from './components/Layout';
-import { Home } from './components/curriculo/Home';
+//import { Home } from './components/curriculo/Home';
 //import { FetchData } from './components/FetchData';
 //import { Counter } from './components/Counter';
+import { Home } from './components/login/Home';
 
 export default class App extends Component {
     static displayName = App.name;
@@ -18,10 +19,14 @@ export default class App extends Component {
         //  </Layout>
         //);
 
+        //return (
+        //    <Layout>
+        //        <Route exact path='/' component={Home} />
+        //    </Layout>
+        //);
+
         return (
-            <Layout>
-                <Route exact path='/' component={Home} />
-            </Layout>
+            <Home />
         );
     }
 }
---
+++ smeCore.SGP/ClientApp/src/components/login/Home.js
@@ -0,0 +1,70 @@
+﻿import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './Home.css';
+
+export class Home extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            collapsed: true
+        };
+
+        this.submitButtonClick = this.submitButtonClick.bind(this);
+    }
+
+    componentDidMount() {
+    }
+
+    componentWillUnmount() {
+    }
+
+    submitButtonClick() {
+        // Desenvolver verificação para checar se os campos estão preenchidos
+        var credential = {
+            username: document.getElementById("usernameTB").value,
+            password: document.getElementById("passwordTB").value
+        };
+
+        alert("Username: " + credential.username + "\nSenha: " + credential.password);
+
+        //fetch('http://smecore.sme.prefeitura.sp.gov.br/api/Auth/LoginJWT')
+        //    .then(response => response.json())
+        //    .then(data => {
+        //        alert(data);
+        //        // Desenvolver rotina para redirecionar o usuário para a página do curriculo/Home.js
+        //    });
+    }
+
+    render() {
+        return (
+            <div className="d-flex justify-content-center align-items-center login-background">
+                <div id="login-logo" className="shadow d-flex justify-content-center align-items-center">
+                    <img src="/img/LogoSGP.svg" alt="sgp icon" id="sgpIcon" />
+                </div>
+
+                <div id="white-board" className="d-flex justify-content-center">
+                    <div id="form">
+                        <div className="d-flex align-items-center">
+                            <i className="fas fa-user login-icon"></i>&nbsp;
+                            <input id="usernameTB" className="form-control login-control" type="text" placeholder="Usuário" />
+                        </div>
+
+                        <div className="vertical-spacing"></div>
+
+                        <div className="d-flex align-items-center">
+                            <i className="fas fa-lock login-icon"></i>&nbsp;
+                            <input id="passwordTB" className="form-control login-control" type="password" placeholder="Senha" />
+                        </div>
+
+                        <div className="vertical-spacing"></div>
+
+                        <div className="d-flex justify-content-center">
+                            <button type="submit" className="btn btn-warning text-white" onClick={this.submitButtonClick}>Entrar</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
