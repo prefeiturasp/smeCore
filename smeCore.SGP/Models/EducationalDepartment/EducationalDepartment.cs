c0dd4c934ca39e6e569b0d1516f83c69e7f1e4db
Massato Kanno <massato.kanno@amcom.com.br>
2019-04-04 12:17:37 -0300

    [Desenvolvimento] - Criacao de hooks e mock de DRE e escola

---
+++ smeCore.SGP/Models/EducationalDepartment/EducationalDepartment.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace smeCore.SGP.Models.EducationalDepartment
+{
+    #region ==================== ATTRIBUTES ====================
+    /// <summary>
+    /// Modelos baseados na documentação do seguinte link https://github.com/victorwss/apis-sme/blob/master/API-escola.md
+    /// acesso dia 03/04/2019, existe uma pequena diferença que foi alinhado, que na API, quando se puxar a DRE, virá também as escolas vinculadas a ela
+    /// O Modelo EducationalDepartment é a DRE
+    /// </summary>
+    public class EducationalDepartment
+    {
+        public string EolCode { get; set; }
+        public string Name { get; set; }
+        public EducationalDepartmentStatus Status { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public DateTime? ExtinctedAt { get; set; }
+        public List<School> Schools { get; set; }
+    }
+    /// <summary>
+    /// Esse modelo representa a Escola ligada a uma DRE
+    /// </summary>
+    public class School
+    {
+        public string EolCode { get; set; }
+        public string InepCode { get; set; }
+        public string PapaCode { get; set; }
+        public string CieCode { get; set; }
+        public string Name { get; set; }
+        public SchoolType Type { get; set; }
+        public SchoolShift Shift { get; set; }
+        public SchoolStatus Status { get; set; }
+        public int Capacity { get; set; }
+        public DateTime CreateddAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public DateTime? ExtinctedAt { get; set; }
+        public EducationalDepartment EducationalDepartment { get; set; }
+    }
+
+    /// <summary>
+    /// Tuno da escola:
+    /// M - Manhã			Aula regular no período da manhã.
+    /// I - Intermediário   Aula das 11:00 às 15:00.
+    /// T - Tarde           Aula regular no período da tarde.
+    /// V - Vespertino      Aula das 15:00 às 19:00.
+    /// G - Integral        Aula de manhã e pela tarde.
+    /// N - Noite           Aula regular no período da noite.
+    /// </summary>
+    public enum SchoolShift
+    {
+        M,
+        I,
+        T,
+        V,
+        G,
+        N
+    }
+
+    /// <summary>
+    /// Status da Escola
+    /// O Created é quando a escola ainda não está em funcionamento
+    /// </summary>
+    public enum SchoolStatus
+    {
+        Created,
+        Active,
+        Extinct
+    }
+
+    /// <summary>
+    /// Tipo de Escola:
+    /// CCA	        - Centro para Crianças e Adolescentes
+    /// CCI/CIPS	- Centro de Convivência Infantil / Centro Integrado de Proteção e Saúde
+    /// CECI	    - Centro de Educação e Cultura Indígena
+    /// CEI DIRET	- Centro de Educação Infantil Direto
+    /// CEI INDIR	- Centro de Educação Infantil Indireto Conveniado
+    /// CEMEI	    - Centro Municipal de Educação Infantil
+    /// CEU	        - Centro Educacional Unificado
+    /// CEU AT COM	- Unidade CEU para atendimento exclusivo de Atividades Complementares
+    /// CEU CEI	    - Centro de Educação Infantil integrante de CEU
+    /// CEU EMEF	- Escola Municipal de Ensino Fundamental integrante de CEU
+    /// CEU EMEI	- Escola Municipal de Educação Infantil integrante de CEU
+    /// CIEJA	    - Centro Integrado de Educação de Jovens e Adultos
+    /// CMCT	    - Centro Municipal de Capacitação e Treinamento
+    /// CR.P.CONV	- Creche Privada Conveniada
+    /// DIR EDUC	- Diretoria Regional de Educação
+    /// E TECNICA	- Escola Técnica
+    /// EMEBS	    - Escola Municipal de Educação Bilíngue para Surdos
+    /// EMEF	    - Escola Municipal de Ensino Fundamental
+    /// EMEFM	    - Escola Municipal de Ensino Fundamental e Médio
+    /// EMEI	    - Escola Municipal de Educação Infantil
+    /// ESC.PART.	- Escola Particular
+    /// ESP CONV	- Especial Conveniada
+    /// MOVA	    - Movimento de Alfabetização de Jovens e Adultos
+    /// OUT-PMSP	- Outras - Prefeitura Municipal de São Paulo
+    /// </summary>
+    public enum SchoolType
+    {
+        CCA,
+        CCI_CIPS,
+        CECI,
+        CEI_DIRET,
+        CEI_INDIR,
+        CEMEI,
+        CEU,
+        CEU_AT_COMP,
+        CEU_CEI,
+        CEU_EMEF,
+        CEU_EMEI,
+        CIEJA,
+        CMCT,
+        CR_P_CONV,
+        DIR_EDUC,
+        E_TECNICA,
+        EMEBS,
+        EMEF,
+        EMEFM,
+        EMEI,
+        ESC_PART,
+        ESP_CONV,
+        MOVA,
+        OUT_PMSP
+    }
+    /// <summary>
+    /// Status da DRE
+    /// </summary>
+    public enum EducationalDepartmentStatus
+    {
+        Active,
+        Extinct
+    }
+    #endregion ==================== ATTRIBUTES ====================
+
+    
+}
---
+++ smeCore.SGP/Models/EducationalDepartment/EducationalDepartmentMock.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace smeCore.SGP.Models.EducationalDepartment
+{
+    public class EducationalDepartmentMock
+    {
+        public List<EducationalDepartment> EducationalDepartments()
+        {
+            List<EducationalDepartment> educationalDepartments = new List<EducationalDepartment>();
+
+            #region ==================== MOCK DRE DIRETORIA REGIONAL DE EDUCACAO GUAIANASE ====================
+            EducationalDepartment educationalDepartment = new EducationalDepartment();
+            educationalDepartment.EolCode = "108500";
+            educationalDepartment.Name = "DIRETORIA REGIONAL DE EDUCACAO GUAIANASES";
+            educationalDepartment.Status = EducationalDepartmentStatus.Active;
+            educationalDepartment.UpdatedAt = Convert.ToDateTime("2019-04-03 15:19:54.597");
+
+            School school = new School();
+            school.EolCode = "019476";
+            school.InepCode = "";
+            school.PapaCode = "";
+            school.CieCode = "";
+            school.Name = "MARIA APARECIDA DO NASCIMENTO, PROFA.";
+            school.Type = SchoolType.EMEF;
+            school.Shift = SchoolShift.G;
+            school.Status = SchoolStatus.Active;
+            school.Capacity = 300;
+            school.CreateddAt = Convert.ToDateTime("2014-02-03 12:27:26.483");
+            school.UpdatedAt = Convert.ToDateTime("2014-02-03 12:27:26.483");
+            educationalDepartment.Schools.Add(school);
+
+            school = new School();
+            school.EolCode = "019455";
+            school.InepCode = "";
+            school.PapaCode = "";
+            school.CieCode = "";
+            school.Name = "CELIA REGINA ANDERY BRAGA, PROF";
+            school.Type = SchoolType.EMEF;
+            school.Shift = SchoolShift.G;
+            school.Status = SchoolStatus.Active;
+            school.Capacity = 200;
+            school.CreateddAt = Convert.ToDateTime("2014-02-03 12:27:26.483");
+            school.UpdatedAt = Convert.ToDateTime("2014-02-03 12:27:26.483");
+            educationalDepartment.Schools.Add(school);
+
+            school = new School();
+            school.EolCode = "009148";
+            school.InepCode = "";
+            school.PapaCode = "";
+            school.CieCode = "";
+            school.Name = "ALEXANDRE VANNUCHI LEME";
+            school.Type = SchoolType.EMEF;
+            school.Shift = SchoolShift.G;
+            school.Status = SchoolStatus.Active;
+            school.Capacity = 100;
+            school.CreateddAt = Convert.ToDateTime("2014-02-03 12:27:26.483");
+            school.UpdatedAt = Convert.ToDateTime("2014-02-03 12:27:26.483");
+            educationalDepartment.Schools.Add(school);
+
+            educationalDepartments.Add(educationalDepartment);
+            #endregion ==================== MOCK DRE DIRETORIA REGIONAL DE EDUCACAO GUAIANASE ====================
+
+
+            #region ==================== DIRETORIA REGIONAL DE EDUCACAO FREGUESIA/BRASILANDIA ====================
+            educationalDepartment = new EducationalDepartment();
+            educationalDepartment.EolCode = "108400";
+            educationalDepartment.Name = "DIRETORIA REGIONAL DE EDUCACAO FREGUESIA/BRASILANDIA";
+            educationalDepartment.Status = EducationalDepartmentStatus.Active;
+            educationalDepartment.UpdatedAt = Convert.ToDateTime("2019-04-03 15:19:54.597");
+
+            school = new School();
+            school.EolCode = "099783";
+            school.InepCode = "";
+            school.PapaCode = "";
+            school.CieCode = "";
+            school.Name = "GERALDO SESSO JUNIOR";
+            school.Type = SchoolType.EMEF;
+            school.Shift = SchoolShift.G;
+            school.Status = SchoolStatus.Active;
+            school.Capacity = 300;
+            school.CreateddAt = Convert.ToDateTime("2014-02-03 12:27:26.483");
+            school.UpdatedAt = Convert.ToDateTime("2014-02-03 12:27:26.483");
+            educationalDepartment.Schools.Add(school);
+
+            school = new School();
+            school.EolCode = "019349";
+            school.InepCode = "";
+            school.PapaCode = "";
+            school.CieCode = "";
+            school.Name = "JARDIM DAMASCENO I";
+            school.Type = SchoolType.EMEF;
+            school.Shift = SchoolShift.G;
+            school.Status = SchoolStatus.Active;
+            school.Capacity = 200;
+            school.CreateddAt = Convert.ToDateTime("2014-02-03 12:27:26.483");
+            school.UpdatedAt = Convert.ToDateTime("2014-02-03 12:27:26.483");
+            educationalDepartment.Schools.Add(school);
+
+            school = new School();
+            school.EolCode = "094358";
+            school.InepCode = "";
+            school.PapaCode = "";
+            school.CieCode = "";
+            school.Name = "ANGELINA MAFFEI VITA, DA.";
+            school.Type = SchoolType.EMEF;
+            school.Shift = SchoolShift.G;
+            school.Status = SchoolStatus.Active;
+            school.Capacity = 100;
+            school.CreateddAt = Convert.ToDateTime("2014-02-03 12:27:26.483");
+            school.UpdatedAt = Convert.ToDateTime("2014-02-03 12:27:26.483");
+            educationalDepartment.Schools.Add(school);
+
+            educationalDepartments.Add(educationalDepartment);
+            #endregion ==================== DIRETORIA REGIONAL DE EDUCACAO FREGUESIA/BRASILANDIA ====================
+
+            return educationalDepartments;
+        }
+
+    }
+}
